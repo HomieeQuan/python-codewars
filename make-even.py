c97d5cfc01b33b4752d753ac8d6f42931af9d6bf
HomieeQuan <middletaq@gmail.com>
2025-05-13 12:07:14 -0400

    day2

---
+++ make-even.py
@@ -0,0 +1,37 @@
+# In this Kata we are passing a number (n) into a function.
+
+# Your code will determine if the number passed is even (or not).
+
+# The function needs to return either a true or false.
+
+# Numbers may be positive or negative, integers or floats.
+
+# Floats with decimal part non equal to zero are considered UNeven for this kata.
+
+# Python has a trick called the modulus operator (%). It tells you the remainder when you divide one number by another.
+
+# If you do number % 2 and get 0, it means no remainder → the number is even!
+
+
+def is_even(n): 
+   return n % 2 == 0
+
+# 1
+def is_even(n):
+   return n % 2 == 0
+
+# 2 
+def is_even(n):
+   return n % 2 == 0
+
+# 3
+def is_even(n):
+   return n % 2 == 0
+
+# 4
+def is_even(n):
+   return n % 2 == 0
+
+# 5
+def is_even(n):
+   return n % 2 == 0
